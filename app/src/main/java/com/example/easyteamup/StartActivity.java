7012a03629c2837bfbb01d1b050ddded1c14cecd
b-go-bell <bridget.go.bell@gmail.com>
2022-03-10 12:27:59 -0800

    start activity done

--- app/src/main/java/com/example/easyteamup/StartActivity.java
+++ app/src/main/java/com/example/easyteamup/StartActivity.java
@@ -2,13 +2,45 @@ package com.example.easyteamup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button loginButton, signupButton;
+    private ProgressBar loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        getSupportActionBar().hide();
+
+        loginButton = (Button) findViewById(R.id.login_button);
+        signupButton = (Button) findViewById(R.id.signup_button);
+
+        loadingBar = (ProgressBar) findViewById(R.id.loading);
+        loadingBar.setVisibility(View.INVISIBLE);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadingBar.setVisibility(View.VISIBLE);
+                Intent login = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(login);
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadingBar.setVisibility(View.VISIBLE);
+                Intent signup = new Intent(StartActivity.this, SignUpActivity.class);
+                startActivity(signup);
+            }
+        });
     }
 }
