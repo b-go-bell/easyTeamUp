9c8cf536e4a48804c6c0313b7994dee08a1ae6e6
Jarret Spino <spino@usc.edu>
2022-03-28 10:49:19 -0700

    Pushing functional FirebaseMessageReceiver.java - will accept messages sent from FB Messenger console. OnNewToken() needs to be implemented.

--- app/src/main/java/com/example/easyteamup/Backend/FirebaseMessageReceiver.java
+++ app/src/main/java/com/example/easyteamup/Backend/FirebaseMessageReceiver.java
@@ -10,6 +10,7 @@ import android.widget.RemoteViews;
 import com.example.easyteamup.Activities.UserAuthActivities.LoginActivity;
 import com.example.easyteamup.R;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -33,45 +34,73 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
         return remoteViews;
     }
 
-    public void showNotification(String title, String message){
-        Intent intent = new Intent(this, LoginActivity.class);
-        String channel_id = "notification channel";
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(
-                this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+    public void showNotification(String title, String message) {
+        System.out.println("title: " + title);
+        System.out.println("message: " + message);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                getApplicationContext(), channel_id)
+        CharSequence name = "test channel";
+        String description = "description channel";
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel("test-id", name, importance);
+        channel.setDescription(description);
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "test-id")
                 .setSmallIcon(R.drawable.clock)
-                .setAutoCancel(true)
-                .setVibrate(new long[]{1111, 1111, 1111, 1111})
-                .setOnlyAlertOnce(true)
-                .setContentIntent(pendingIntent);
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-        if (Build.VERSION.SDK_INT
-                >= Build.VERSION_CODES.JELLY_BEAN) {
-            builder = builder.setContent(
-                    getCustomDesign(title, message));
-        }
-        else {
-            builder = builder.setContentTitle(title)
-                    .setContentText(message)
-                    .setSmallIcon(R.drawable.clock);
-        }
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        if (Build.VERSION.SDK_INT
-                >= Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel
-                    = new NotificationChannel(
-                    channel_id, "web_app",
-                    NotificationManager.IMPORTANCE_HIGH);
-            notificationManager.createNotificationChannel(
-                    notificationChannel);
-        }
-
-        notificationManager.notify(0, builder.build());
+// notificationId is a unique int for each notification that you must define
+        notificationManagerCompat.notify(69, builder.build());
     }
+
+
 }
+
+//    public void showNotification(String title, String message){
+//        System.out.println("title: " + title);
+//        System.out.println("message: " + message);
+//        Intent intent = new Intent(this, LoginActivity.class);
+//        String channel_id = "notification channel";
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(
+//                this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+//
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+//                getApplicationContext(), channel_id)
+//                .setSmallIcon(R.drawable.clock)
+//                .setAutoCancel(true)
+//                .setVibrate(new long[]{1111, 1111, 1111, 1111})
+//                .setOnlyAlertOnce(true)
+//                .setContentIntent(pendingIntent);
+//
+//        if (Build.VERSION.SDK_INT
+//                >= Build.VERSION_CODES.JELLY_BEAN) {
+//            builder = builder.setContent(
+//                    getCustomDesign(title, message));
+//        }
+//        else {
+//            builder = builder.setContentTitle(title)
+//                    .setContentText(message)
+//                    .setSmallIcon(R.drawable.clock);
+//        }
+//
+//        NotificationManager notificationManager =
+//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//
+//        if (Build.VERSION.SDK_INT
+//                >= Build.VERSION_CODES.O) {
+//            NotificationChannel notificationChannel
+//                    = new NotificationChannel(
+//                    channel_id, "web_app",
+//                    NotificationManager.IMPORTANCE_HIGH);
+//            notificationManager.createNotificationChannel(
+//                    notificationChannel);
+//        }
+//
+//        notificationManager.notify(0, builder.build());
+//    }
